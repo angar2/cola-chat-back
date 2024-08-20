0157e92246457a216e61b3fee62bb24d0691afd3
angar2 <uky00832@gmail.com>
2024-08-21 01:15:35 +0900

    chore: set up configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  const config = app.get(ConfigService);
+  const PORT = config.get<number>('PORT');
+
+  await app.listen(PORT);
 }
 bootstrap();
