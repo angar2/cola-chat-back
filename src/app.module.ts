9b2ed425fab262033033452ecff4d5f2c5b4b69d
angar2 <uky00832@gmail.com>
2024-08-21 03:09:31 +0900

    feat: add chat module with websocket gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`,
     }),
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
