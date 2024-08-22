575b6ab82bc6680059a002cd819eb1cb09c1e997
angar2 <uky00832@gmail.com>
2024-08-22 20:28:26 +0900

    chore: set cors origin

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   const PORT = config.get<number>('PORT');
+  const CORS_ORIGIN_DEV = config.get<string>('CORS_ORIGIN_DEV');
+  const CORS_ORIGIN_PROD = config.get<string>('CORS_ORIGIN_PROD');
+
+  app.enableCors({
+    origin: [CORS_ORIGIN_DEV, CORS_ORIGIN_PROD],
+    credentials: true,
+  });
 
   await app.listen(PORT);
 }
