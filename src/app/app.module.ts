35d2334a959fd48afd14fdfe1deff08413aba82e
angar2 <uky00832@gmail.com>
2024-09-10 18:14:06 +0900

    refactor: update project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './shared/prisma/prisma.module';
-import { RedisModule } from './shared/redis/redis.module';
+import { PrismaModule } from '../shared/prisma/prisma.module';
+import { RedisModule } from '../shared/redis/redis.module';
 import { ChatModule } from './chat/chat.module';
-import { ApiErrorExceptionFilter } from './shared/filters/api-errorException.Filter';
-import { ApiResponseInterceptor } from './shared/interceptors/api-response.interceptor';
+import { ApiErrorExceptionFilter } from '../shared/filters/api-errorException.Filter';
+import { ApiResponseInterceptor } from '../shared/interceptors/api-response.interceptor';
 
 @Module({
   imports: [
